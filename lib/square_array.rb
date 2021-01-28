fabac3a1fe21d4778fb32307c48772aa33b58e5d
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 17:13:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers)
   numbers = [1,2,3]
+  new_numbers = [9,10,16,25]
 
-  Integer.sqrt(numbers)
+  message.sqrt do |index|
+    puts message[index]
+  end
 end
-
-p square_array
