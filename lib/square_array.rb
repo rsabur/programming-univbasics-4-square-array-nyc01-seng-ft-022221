63360ab501030e5c4c31aa2ac8ffeb9951b6c1ce
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 17:33:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   numbers = [1,2,3]
   new_numbers = [9,10,16,25]
 
-  numbers.length.times do |index|
+  numbers.length do |index|
     new_numbers.push(numbers[index] ** 2)
   end
   new_numbers
